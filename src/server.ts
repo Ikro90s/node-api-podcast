8943df97eff47b5d350c122ba596eabedfd61c63
Icaro Pereira De Paula <icaropereiradepaula@gmail.com>
2026-03-31 07:48:02 -0400

    Separação de responsabilidades, da configuração inicial do servidor e inicialização do app

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,8 @@
 import * as http from 'http';
-import {
-  getFilterEpisodes,
-  getListEpisodes,
-} from './controllers/podcasts-controller.js';
-import { Routes } from './routes/routes.js';
-import { HttpMethods } from './utils/http-methods.js';
+import app from './app.js';
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(
-  async (request: http.IncomingMessage, response: http.ServerResponse) => {
-    const [baseUrl] = request.url?.split('?') || [];
-    console.log('baseUrl:', baseUrl);
-    //listar podcasts
-    if (request.method === HttpMethods.GET && baseUrl === Routes.LIST)
-      await getListEpisodes(request, response);
-    //filtrar podcasts
-    if (request.method === HttpMethods.GET && baseUrl === Routes.FILTER)
-      await getFilterEpisodes(request, response);
-  },
-);
+const server = http.createServer(app);
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
